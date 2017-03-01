16bb6f60673a5cb218ac8a9d5c0187a88b5aa349
yuriykots <setingm@gmail.com>
2017-03-01 17:42:46 -0500

    Adding express server file

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const knex = require("knex")
+
+//Now we will configure database connection.
+
+const db = knex({
+  client: "mysql",
+  connection:{
+    host:"127.0.0.1",
+    user: "root",
+    database: "todo_list",
+  }
+})
+
+express()
+  .use(bodyParser.json())
+  .get("/", (req,res) =>{
+    db("users").then((data) => {
+      res.send(data)
+    })
+  })
+  .listen(3000)
+
+
+  //.use(bodyParser.json())
