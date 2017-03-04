633bce9b7eaaaa473deec41a18fe9c9994d20d16
yuriykots <setingm@gmail.com>
2017-03-04 00:59:12 -0500

    Render React components on server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const knex = require("knex")
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Layout from './js/Components/Layout'
 
 //Now we will configure database connection.
 
@@ -17,15 +20,34 @@ var app = express()
 
 app.use(bodyParser.json())
 app.get("/", (req,res) =>{
-    db("users").then((data) => {
-      res.send("hello bro")
-     })
-  })
+      const markup = renderToString(<Layout/>)
 
-
-var PORT = process.env.PORT || 3000
-app.listen(PORT, function() {
-    console.log('Production Express server running at localhost:' + PORT)
+    // dump the HTML into a template, lots of ways to do this, but none are
+    // really influenced by React Router, so we're just using a little
+    // function, `renderPage`
+      res.send(renderPage(markup))
 })
 
-  //.use(bodyParser.json())
+/*app.get("/", (req,res) =>{
+    db("users").then((data) => {
+      res.send(data)
+    })
+})
+*/
+
+function renderPage(markup) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app> ${markup} </div>
+    <script src="/bundle.js"></script>
+   `
+}
+
+var PORT = process.env.PORT || 3000
+  app.listen(PORT, function() {
+    console.log('Production Express server running at localhost:' + PORT)
+})
